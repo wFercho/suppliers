2714651e32d385ceb820d06fe40cd1d088b6dc60
Fernando Salamanca <fesaba21@gmail.com>
2024-04-20 18:29:47 -0500

    feat: handle add supplier supplie

---
+++ api/supplier_supplies_handler.go
@@ -0,0 +1,25 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	e "suppliers/entities"
+)
+
+func (s *APIServer) handleAddSupplierSupplie(w http.ResponseWriter, r *http.Request) error {
+
+	createSSrequest := new(CreateSupplierSupplierRequest)
+
+	if err := json.NewDecoder(r.Body).Decode(createSSrequest); err != nil {
+		return err
+	}
+
+	defer r.Body.Close()
+
+	ss := e.NewSupplierSupplie(createSSrequest.Name, createSSrequest.SupplierID, createSSrequest.SupplieID)
+	if err := s.store.CreateSupplierSupplie(ss); err != nil {
+		return err
+	}
+
+	return WriteJSON(w, http.StatusCreated, ss)
+}
